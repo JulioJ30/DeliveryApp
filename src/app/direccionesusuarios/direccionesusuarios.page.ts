c0799c026904df5d08f7424c062c4dc7df8de2cb
JAMORETTI <jamoretti@tsc.com.pe>
2020-05-20 21:09:26 -0500

    cambios de estilos

--- src/app/direccionesusuarios/direccionesusuarios.page.ts
+++ src/app/direccionesusuarios/direccionesusuarios.page.ts
@@ -27,7 +27,7 @@ export class DireccionesusuariosPage implements OnInit {
   constructor(private geolocation:Geolocation,private platform: Platform,private modalCrtl:ModalController,private maps:GoogleMapsService) { 
 
     this.platform.ready().then(()=>{
-      this.maps.MostrarMapaIndividual('map',this.lat ,this.lng,"Plaza  de Armas");
+      this.maps.MostrarMapaIndividual('map2',this.lat ,this.lng,"Plaza  de Armas");
     });
 
   }
@@ -42,17 +42,17 @@ export class DireccionesusuariosPage implements OnInit {
 
     // use geolocation to get user's device coordinates
     getCurrentCoordinates() {
-      this.maps.MostrarMapaIndividual('map',-13.440235707136699 ,-76.14049022833623,"Plaza  de Armas");
+      // this.maps.MostrarMapaIndividual('map2',-13.440235707136699 ,-76.14049022833623,"Plaza  de Armas");
 
-      // this.geolocation.getCurrentPosition(this.options).then((resp) => {
-      //   this.lat = resp.coords.latitude;
-      //   this.lng = resp.coords.longitude;
-      //   this.maps.MostrarMapaIndividual('map',this.lat ,this.lng,"Plaza  de Armas");
-      //   // -13.440235707136699, -76.14049022833623
+      this.geolocation.getCurrentPosition(this.options).then((resp) => {
+        this.lat = resp.coords.latitude;
+        this.lng = resp.coords.longitude;
+        this.maps.MostrarMapaIndividual('map',this.lat ,this.lng,"Plaza  de Armas");
+        // -13.440235707136699, -76.14049022833623
 
-      //  }).catch((error) => {
-      //    console.log('Error getting location', error);
-      //  });
+       }).catch((error) => {
+         console.log('Error getting location', error);
+       });
     }
 
  
